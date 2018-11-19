e9c42b7b39e8ebf20d876b30eb79460c79e3d173
Eveline Meyer <floo@devzone.ch>
2018-11-19 21:24:27 +0100

    modified Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,9 @@ public class Greeter {
   }
 
   /**
-   * Greeting method
    * 
+   * @param someone String
+   * @return greeting String
    */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
