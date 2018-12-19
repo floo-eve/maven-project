34239e651e8d62e41657a14b725ee30d0508d293
Eveline Meyer <floo@devzone.ch>
2018-12-19 21:06:58 +0100

    fixed checkstyle problem

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -18,7 +18,7 @@ public class Greeter {
    * @param someone String
    * @return greeting String
    */
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
